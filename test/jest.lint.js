d4689ab3a44cf89f43e1e817e449802aa5a5404f
Nam Nguyen <nam.nguyen.node@gmail.com>
2018-12-28 15:15:55 +0700

    Fix jest-runner-eslint configuration

--- test/jest.lint.js
+++ test/jest.lint.js
@@ -1,7 +1,7 @@
 const path = require('path')
 module.exports = {
   rootDir: path.join(__dirname, '..'),
-  runner: 'jest-runner-eslint --fix',
+  runner: 'jest-runner-eslint',
   displayName: 'lint',
   testPathIgnorePatterns: ['node_modules', 'dist', 'coverage', 'test'],
   testMatch: ['**/*.+(js|ts|tsx)'],
